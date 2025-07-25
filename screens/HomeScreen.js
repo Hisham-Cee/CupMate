b4e6c4df779c5af7974643f6afd1da306ba9b8eb
Hisham Mohammed <hishamcee@gmail.com>
2025-07-25 07:20:24 +0530

    Home screen interface design completed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,157 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Image, Pressable, ScrollView, StyleSheet, Text, TextInput, View, FlatList } from "react-native";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Card from "../components/Card";
+import { LinearGradient } from 'expo-linear-gradient';
+import { useState } from "react";
+
 
 function HomeScreen(){
+    const [selectedCategory, setSelectedCategory] = useState("Cappuccino");
+    const categories = ["Cappuccino", "Macchiato", "Latte", "Americano", "Mochas"];
+
+    const cardData = [
+        {
+            id: '1',
+            imageSource: require('../assets/images/Frame1.png'),
+            rating: '4.8',
+            drinkName: 'Cappuccino',
+            desc: 'with Chocolate',
+            amount: '4.53',
+        },
+        {
+            id: '2',
+            imageSource: require('../assets/images/Frame2.png'),
+            rating: '4.9',
+            drinkName: 'Cappuccino',
+            desc: 'with Oat Milk',
+            amount: '3.90',
+        },
+        {
+            id: '3',
+            imageSource: require('../assets/images/Frame3.png'),
+            rating: '4.5',
+            drinkName: 'Cappuccino',
+            desc: 'with Chocolate',
+            amount: '4.53',
+        },
+        {
+            id: '4',
+            imageSource: require('../assets/images/Frame4.png'),
+            rating: '4.0',
+            drinkName: 'Cappuccino',
+            desc: 'with Chocolate',
+            amount: '4.53',
+        },
+        {
+            id: '5',
+            imageSource: require('../assets/images/Cappuccino.png'),
+            rating: '4.8',
+            drinkName: 'Cappuccino',
+            desc: 'with Chocolate',
+            amount: '4.53',
+        },
+        {
+            id: '6',
+            imageSource: require('../assets/images/Cappuccino.png'),
+            rating: '4.8',
+            drinkName: 'Cappuccino',
+            desc: 'with Chocolate',
+            amount: '4.53',
+        },
+    ];
+
     return(
-        <View style={styles.rootContainer}>
-            <Text style={styles.text}>Welcome!</Text>
-        </View>
+        <>
+        <StatusBar style="light" />
+            <View style={styles.rootContainer}>
+                <View style={styles.topContainer}>
+                    <LinearGradient
+                        colors={['#1e1c1cff', 'black']}
+                        start={{ x: 0.5, y: 0 }}
+                        end={{ x: 0.5, y: 1 }}
+                        style={StyleSheet.absoluteFill}
+                    />
+                    <View style={styles.headerContainer}>
+                        <View>
+                            <Text style={styles.labelText}>Location</Text>
+                            <Text style={styles.locationText}>Bilzen, Tanjungbalai v</Text>
+                        </View>
+                        <Pressable>
+                            <Ionicons name="person-circle-outline" size={45} color='#fff'/>
+                        </Pressable>
+                    </View>
+                    <View style={styles.searchContainer}>
+                        <Ionicons name="search" size={25} color="#fff" />
+                        <TextInput
+                            placeholder="Search coffee"
+                            placeholderTextColor="#aaa"
+                            style={styles.searchInput}
+                        />
+                        <Pressable style={styles.optionButton}>
+                            <Ionicons name="options" size={20} color="#fff" />
+                        </Pressable>
+                    </View>
+                </View>
+                <View style={styles.imgContainer}>
+ 
+                    <View style={StyleSheet.absoluteFill}>
+                        <View style={{ flex: 1, backgroundColor: 'black' }} />
+                        <View style={{ flex: 1, backgroundColor: 'white' }} />
+                    </View>
+
+                    <Image
+                        source={require('../assets/images/Frame.png')}
+                        style={styles.promoCard}
+                    />
+                </View>
+
+                <View style={styles.lightSection}>
+                    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                        {categories.map((category) => (
+                            <Pressable
+                                key={category}
+                                style={[
+                                    styles.categoriesTab,
+                                    selectedCategory === category && styles.selectedTab,
+                                ]}
+                                onPress={() => setSelectedCategory(category)}
+                            >
+                                <Text
+                                    style={[
+                                        styles.tabText,
+                                        selectedCategory === category && styles.selectedTabText,
+                                    ]}
+                                >
+                                    {category}
+                                </Text>
+                            </Pressable>
+                        ))}
+                    </ScrollView>
+                </View>
+                
+                <View style={styles.lightSection2} >
+                    <FlatList
+                        style={{flex: 1}}
+                        data={cardData}
+                        keyExtractor={(item) => item.id}
+                        renderItem={({ item }) => (
+                            <Card
+                                imageSource={item.imageSource}
+                                rating={item.rating}
+                                drinkName={item.drinkName}
+                                desc={item.desc}
+                                amount={item.amount}
+                            />
+                        )}
+                        numColumns={2}
+                        columnWrapperStyle={{ justifyContent: 'space-between' }}
+                        showsVerticalScrollIndicator={false}
+                        contentContainerStyle={{ paddingBottom: 30 }}
+                    />
+                </View>
+            </View>
+        </>
     );
 };
 
@@ -13,10 +160,101 @@ export default HomeScreen;
 const styles = StyleSheet.create({
     rootContainer:{
         flex: 1,
+        backgroundColor: '#fffefeff',
+    },
+     topContainer: {
+        backgroundColor: '#191818ff',
+        paddingTop: 60,
+        paddingHorizontal: 30,
+        paddingBottom: 10,
+    },
+    headerContainer:{
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    labelText: {
+        color: '#B7B7B7',
+        fontSize: 12,
+    },
+    locationText: {
+        color: '#fff',
+        fontSize: 14,
+    },
+    locationContainer:{
+        flexDirection: 'column'
+    },
+    profileText: {
+        color: '#fff',
+    },
+    searchContainer:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#292928ff',
+        borderRadius: 10,
+        paddingLeft: 12,
+        paddingRight: 4,
+        height: 56,
+        marginTop: 10,
+    },
+    searchInput:{
+        flex: 1,
+        marginHorizontal: 2,
+        color: '#fff',
+    },
+    optionButton:{
+        backgroundColor: '#C67C4E',
+        width: 45,
+        height: 45,
+        borderRadius: 10,
         justifyContent: 'center',
         alignItems: 'center',
+        
     },
-    text:{
-        fontSize: 30
-    }
+     lightSection: {
+        marginHorizontal: 30,
+        paddingTop:0,
+    },
+    lightSection2: {
+        flex:1,
+        paddingHorizontal: 30,
+        paddingTop:10,
+    },
+    imgContainer:{
+        height: 200,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+        overflow: 'hidden',
+    },
+    promoCard:{
+        width: '85%',
+        height: '85%',
+        resizeMode: 'contain',
+    },
+    categoryScroll: {
+        
+    },
+    categoriesTab:{
+       backgroundColor: 'hsla(20, 60%, 98%, 1.00)',
+        paddingHorizontal: 16,
+        paddingVertical: 6,
+        borderRadius: 12,
+        marginRight: 10,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    selectedTab: {
+        backgroundColor: '#c5845bef', 
+    },
+    tabText: {
+        color: '#333',
+        fontWeight: '500',
+    },
+    selectedTabText: {
+        color: '#fff', 
+        fontWeight: '700',
+    }   
 });
